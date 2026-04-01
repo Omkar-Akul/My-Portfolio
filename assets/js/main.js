d29a32c31d3ca7f07e53d01e07d9a956348e21df
Omkar-Akul <omkarakul1907@gmail.com>
2026-04-01 15:30:59 +0530

    p

---
+++ assets/js/main.js
@@ -0,0 +1,133 @@
+/*=============== HOME SPLIT TEXT ===============*/
+const { animate,splitText, stagger } = anime
+
+const { chars:chars1 } = splitText('.home__profession-1', {
+  chars: true
+});
+const { chars:chars2 } = splitText('.home__profession-2', {
+  chars: true
+});
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+animate(chars2, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+
+
+/*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects__swiper', {
+  
+  loop: true,
+  spaceBetween:24,
+  slidesPerView:'auto',
+  grabCursor:true,
+  speed:600,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+
+  autoplay: {
+    delay:3000,
+    disableOnInteraction:false,
+  }
+})
+
+/*=============== WORK TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach((tab)=> {
+  tab.addEventListener('click', () =>{
+    const targetSelector = tab.dataset.target,
+          targetContent = document.querySelector(targetSelector)
+
+      //disable
+      tabContents.forEach((content) => content.classList.remove('work-active'))
+      tabs.forEach((t) => t.classList.remove('work-active'))
+
+      //active
+      tab.classList.add('work-active')
+      targetContent.classList.add('work-active')
+  })
+})
+
+/*=============== SERVICES ACCORDION ===============*/
+const ServicesButtons =  document.querySelectorAll('.services__button')
+
+ServicesButtons.forEach(button => {
+
+  const heightInfo = document.querySelector('.services__info')
+  heightInfo.style.height = heightInfo.scrollHeight + 'px'
+
+  button.addEventListener('click', () => {
+    const servicesCards = document.querySelectorAll('.services__card'),
+    currentCard =  button.parentNode,
+    currentInfo  = currentCard.querySelector('.services__info'),
+    isCardOpen = currentCard.classList.contains('services-open')
+
+
+    servicesCards.forEach(card => {
+      card.classList.replace('services-open' , 'services-close')
+
+      const info = card.querySelector('.services__info')
+      info.style.height = '0'
+
+
+      if(!isCardOpen){
+        currentCard.classList.replace('services-close', 'services-open')
+        currentInfo.style.height = currentInfo.scrollHeight + 'px'
+      }
+
+    })
+  })
+})
+
+/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+
+
+/*=============== COPY EMAIL IN CONTACT ===============*/
+const copyBtn = document.getElementById('contact-btn'),
+      copyEmail = document.getElementById('contact-email').textContent
+
+copyBtn.addEventListener('click', ()=> {
+  navigator.clipboard.writeText(copyEmail).then(()=>{
+    copyBtn.innerHTML = 'Email Copied <i class="ri-check-line"> </i>'
+
+
+    setTimeout(() => {
+      copyBtn.innerHTML = 'copy email<i class="ri-file-copy-line"></i>'
+    },2000)
+  })
+  
+})
+
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== CUSTOM CURSOR ===============*/
+
+
+/* Hide custom cursor on links */
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
